54fa0ca8029aec0dc4295c536cdcb8ecc33c55ae
VasekParkhimovich <parkhimovichvasiliy@gmail.com>
2022-06-11 15:42:09 +0300

    New List

---
+++ src/com/second/Main.java
@@ -0,0 +1,52 @@
+package com.second;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        try {
+            ArrayList<MyObj> list = new ArrayList<MyObj>();
+            ArrayList<String> nameList = new ArrayList<String>();
+            nameList.add("Василий");
+            nameList.add("Дмитрий");
+            nameList.add("Ростислав");
+            nameList.add("Татьяна");
+            nameList.add("Анастасия");
+            nameList.add("Юлия");
+            System.out.println(nameList.size());
+
+            Iterator itName = nameList.iterator();
+            while (itName.hasNext()) {
+                for (int i=1;i<(nameList.size())+1; i++) {
+                    String str = (String) itName.next();
+                    list.add(new MyObj(str, i));
+                }
+            }
+            Iterator itShow = list.iterator();
+            for (int i = 1; i < 7; i++) {
+                while (itShow.hasNext()) {
+                    MyObj a = (MyObj) itShow.next();
+                    a.show();
+                }
+            }
+        }catch(NoSuchElementException e){
+            System.out.println("Error "+ e);
+        }
+    }
+}
+class MyObj{
+    static int count = 0;
+    int num;
+    String name;
+    MyObj(String name, int num){
+        this.name = name;
+        this.num = num;
+        count++;
+    }
+    void show(){
+        System.out.println("Имя обьекта номер "+num+":"+name+".");
+    }
+}
+
+
