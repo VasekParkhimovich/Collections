9d8507faa3b3d19fd1112e87ea0be7b77b898f89
VasekParkhimovich <parkhimovichvasiliy@gmail.com>
2022-06-13 19:47:26 +0300

    играюсь с HashMap

--- src/com/second/Main.java
+++ src/com/second/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        NameList nameList = new NameList();
-        nameList.showName();
+
+        new MyHashMap();
 
     }
 }
@@ -24,7 +24,7 @@ class MyObj{
     }
 }
 class NameList{
-    void showName(){
+    NameList(){
         try {
             ArrayList<MyObj> list = new ArrayList<MyObj>();
             ArrayList<String> nameList = new ArrayList<String>();
@@ -60,8 +60,49 @@ class NameList{
     }
 }
 class MyHashMap{
-    
+    MyHashMap(){
+        HashMap<Integer,String> hm = new HashMap<>();
+        HashMap<Integer,String> hm2 = new HashMap<>(hm);
+
+        hm.put(1,"Peugeot");
+        hm.put(2,"Opel");
+        hm.put(3,"Lada");
+        hm.put(4,"Lamborgini");
+        hm.put(5,"Mercedes");
+        hm.put(6,"BMW");
+
+        hm.replace(3,"Tesla");
+
+        System.out.println(hm2);
+        hm2.putAll(hm);
+        System.out.println(hm2);
+
+        hm2.put(5,"Renault");
+        hm2.put(6,"Ford");
+        hm2.put(7,"Jeep");
+        hm2.put(8,"Audi");
+        hm2.put(9,"VW");
+        hm2.put(10,"Porsche");
+        System.out.println(hm2);
+
+        List ar = new ArrayList(hm2.values());
+
+        Set mapSet = hm2.entrySet();
+        Iterator setIt = mapSet.iterator();
+        Iterator arIt = ar.iterator();
+        HashMap hm3 = new HashMap();
+        hm3 = (HashMap) hm2.clone();
+        System.out.println(mapSet);
+
+
+        boolean b = hm.isEmpty();
+
+        String str = b?"True":"False";
+        System.out.println(str);
+
+    }
 }
+
 
 
 
